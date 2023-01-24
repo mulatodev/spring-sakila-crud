7b98fa39df0f96daf08839824a7e67133401bd89
mulatodev <ganaranjo@gmail.com>
2023-01-24 15:43:29 -0500

    Se adicionan archivos iniciales del proyecto

---
+++ src/test/java/com/arcadia/samples/springsakilacrud/SpringSakilaCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arcadia.samples.springsakilacrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSakilaCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
