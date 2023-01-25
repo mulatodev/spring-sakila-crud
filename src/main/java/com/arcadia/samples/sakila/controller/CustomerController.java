3f6b5191d73a7f08f4ee7968c85539fb94fd3d4f
mulatodev <ganaranjo@gmail.com>
2023-01-25 16:32:12 -0500

    Se modifica controladora para formulario de registro de un cliente. Se crea pagina html para formulario

--- src/main/java/com/arcadia/samples/sakila/controller/CustomerController.java
+++ src/main/java/com/arcadia/samples/sakila/controller/CustomerController.java
@@ -1,5 +1,6 @@
 package com.arcadia.samples.sakila.controller;
 
+import com.arcadia.samples.sakila.model.CustomerModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,5 +23,13 @@ public class CustomerController {
     public String listCustomers(Model model){
         model.addAttribute("customers", service.getAllCustomers());
         return "customers";  
+    }
+    
+    @GetMapping("/customers/new")
+    public String createCustomer(Model model){
+        CustomerModel customerModel = new CustomerModel();
+        
+        model.addAttribute("customer", customerModel);
+        return "customers_add";
     }
 }
